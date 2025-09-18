f26ba954e8122340f2381a368d19553cb76c96de
ngoctanz <lnt.ngoctan@gmail.com>
2025-09-18 19:58:04 +0700

    update bai 1

--- bai1/src/Class/PhanSo.java
+++ bai1/src/Class/PhanSo.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 public class PhanSo {
 	// thuoc tinh
-	private int tuSo_249;
-	private int mauSo_249;
+	private int tuSo_236;
+	private int mauSo_236;
 
 	// phuong thuc
 	// ham khoi tao khong doi so
 	public PhanSo() {
-		tuSo_249 = 0;
-		mauSo_249 = 1;
+		tuSo_236 = 0;
+		mauSo_236 = 1;
 	}
 
 	// ham khoi tao co doi so
 	public PhanSo(int tuSo, int mauSo) {
-		this.tuSo_249 = tuSo;
-		this.mauSo_249 = mauSo;
+		this.tuSo_236 = tuSo;
+		this.mauSo_236 = mauSo;
 	}
 
 	// ham nhap phan so
@@ -36,49 +36,49 @@ public class PhanSo {
 			if (b == 0) {
 				System.out.println("Mau so khong duoc bang 0. Hay nhap lai!");
 			} else {
-				tuSo_249 = a;
-				mauSo_249 = b;
+				tuSo_236 = a;
+				mauSo_236 = b;
 			}
 		} while (b == 0);
 	}
 
 	// ham hien thi
 	public void hienThiPS() {
-		if (tuSo_249 * mauSo_249 < 0) {
-			System.out.println("\t-" + Math.abs(tuSo_249) + "/" + Math.abs(mauSo_249 ));
+		if (tuSo_236 * mauSo_236 < 0) {
+			System.out.println("\t-" + Math.abs(tuSo_236) + "/" + Math.abs(mauSo_236 ));
 		} else {
-			System.out.println("\t" + Math.abs(tuSo_249) + "/" + Math.abs(mauSo_249 ));
+			System.out.println("\t" + Math.abs(tuSo_236) + "/" + Math.abs(mauSo_236 ));
 		}
 	}
 
 	// ham cong
 	public PhanSo congPS(PhanSo ps2) {
-		int a = tuSo_249 * ps2.mauSo_249 + ps2.tuSo_249 * mauSo_249 ;
-		int b = mauSo_249 * ps2.mauSo_249 ;
+		int a = tuSo_236 * ps2.mauSo_236 + ps2.tuSo_236 * mauSo_236 ;
+		int b = mauSo_236 * ps2.mauSo_236 ;
 
 		return new PhanSo(a, b);
 	}
 
 	// ham tru
 	public PhanSo truPS(PhanSo ps2) {
-		int a = tuSo_249 * ps2.mauSo_249 - ps2.tuSo_249 * mauSo_249 ;
-		int b = mauSo_249 * ps2.mauSo_249 ;
+		int a = tuSo_236 * ps2.mauSo_236 - ps2.tuSo_236 * mauSo_236 ;
+		int b = mauSo_236 * ps2.mauSo_236 ;
 
 		return new PhanSo(a, b);
 	}
 
 	// ham nhan
 	public PhanSo nhanPS(PhanSo ps2) {
-		int a = tuSo_249 * ps2.tuSo_249;
-		int b = mauSo_249 * ps2.mauSo_249 ;
+		int a = tuSo_236 * ps2.tuSo_236;
+		int b = mauSo_236 * ps2.mauSo_236 ;
 
 		return new PhanSo(a, b);
 	}
 
 	// ham chia
 	public PhanSo chiaPS(PhanSo ps2) {
-		int a = tuSo_249 * ps2.mauSo_249 ;
-		int b = mauSo_249 * ps2.tuSo_249;
+		int a = tuSo_236 * ps2.mauSo_236 ;
+		int b = mauSo_236 * ps2.tuSo_236;
 
 		return new PhanSo(a, b);
 	}
@@ -98,7 +98,7 @@ public class PhanSo {
 
 	// ham kiem tra phan so toi gian hay chua
 	public boolean kiemTraToiGian() {
-		if (timUSCLN(tuSo_249, mauSo_249 ) == 1) {
+		if (timUSCLN(tuSo_236, mauSo_236 ) == 1) {
 			return true;
 		}
 		return false;
@@ -106,9 +106,9 @@ public class PhanSo {
 
 	// ham toi gian
 	public void toiGianPS() {
-		int x = timUSCLN(tuSo_249, mauSo_249 );
+		int x = timUSCLN(tuSo_236, mauSo_236 );
 
-		tuSo_249 /= x;
-		mauSo_249 /= x;
+		tuSo_236 /= x;
+		mauSo_236 /= x;
 	}
 }
